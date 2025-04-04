2bb7326d10eacbe2417f510220f681cf1065ed79
Neda Kaighobadi <neda.kaighobadi@gmail.com>
2025-04-04 10:38:41 +0300

    add rules for file issues

--- src/tools/listFileIssuesTool.ts
+++ src/tools/listFileIssuesTool.ts
@@ -1,9 +1,30 @@
 import { Tool } from '@modelcontextprotocol/sdk/types.js';
-import { defaultPagination, fileSchema } from '../utils.js';
+import {
+  defaultPagination,
+  fileSchema,
+  generalOrganizationMistakes,
+  generalRepositoryMistakes,
+} from '../utils.js';
+import { listFilesTool } from './listFilesTool.js';
+
+const rules = `
+  Use this tool to list the issues for specific files in a repository. 
+
+  Common use cases: 
+  - When the user asks for the issues for a specific file
+  - When the user is in a file context and asks for the issues
+
+  Common mistakes: 
+  - Using this tool for listing the issues for specific pull requests
+  - Using this tool for listing the issues for an entire repository
+  - Using this with a wrong fileId (validate the fileId with the ${listFilesTool.name} tool)
+  ${generalOrganizationMistakes}
+  ${generalRepositoryMistakes}
+`;
 
 export const listFileIssuesTool: Tool = {
   name: 'codacy_get_file_issues',
-  description: 'Get the issue list for a file in a repository',
+  description: `Get the issue list for a file in a repository. \n ${rules}`,
   inputSchema: {
     type: 'object',
     properties: {
