cd4cc0251068ce768e7465fbf0142a37f782a10d
Alejandro Rizzo <alejandro.rizzo@codacy.com>
2025-05-20 09:33:22 +0000

    feat: CLI refactor + Windows support (#59)
    * feat: refactor CLI logic
    
    * debug windows session changes
    
    * fix working on wsl or non wsl sessions
    
    * fix wsl check
    
    * clean up code
    
    * review

---
+++ src/cli/CodacyCli.ts
@@ -0,0 +1,82 @@
+export const CODACY_FOLDER_NAME = '.codacy';
+import { exec } from 'child_process';
+import { Log } from 'sarif';
+
+// Set a larger buffer size (10MB)
+const MAX_BUFFER_SIZE = 1024 * 1024 * 10;
+
+export abstract class CodacyCli {
+  private _cliCommand: string = '';
+
+  public readonly _accountToken = process.env.CODACY_ACCOUNT_TOKEN;
+  public readonly _cliVersion = process.env.CODACY_CLI_VERSION;
+
+  public readonly rootPath: string;
+  public readonly provider?: string;
+  public readonly organization?: string;
+  public readonly repository?: string;
+
+  constructor(rootPath: string, provider?: string, organization?: string, repository?: string) {
+    this.rootPath = rootPath;
+    this.provider = provider;
+    this.organization = organization;
+    this.repository = repository;
+  }
+
+  public abstract install(): Promise<void>;
+  public abstract installDependencies(): Promise<void>;
+
+  public abstract update(): Promise<void>;
+  public abstract initialize(): Promise<void>;
+
+  public abstract analyze(options: { file?: string; tool?: string }): Promise<Log | null>;
+
+  protected getCliCommand(): string {
+    return this._cliCommand;
+  }
+  protected setCliCommand(command: string): void {
+    this._cliCommand = command;
+  }
+
+  protected preparePathForExec(path: string): string {
+    return path;
+  }
+
+  protected execAsync(
+    command: string,
+    args?: Record<string, string>
+  ): Promise<{ stdout: string; stderr: string }> {
+    // stringyfy the args
+    const argsString = Object.entries(args || {})
+      .map(([key, value]) => `--${key} ${value}`)
+      .join(' ');
+
+    // Add the args to the command and remove any shell metacharacters
+    const cmd = `${command} ${argsString}`.trim().replace(/[;&|`$]/g, '');
+
+    // Add the CODACY_CLI_VERSION to the command
+    return new Promise((resolve, reject) => {
+      exec(
+        cmd,
+        {
+          cwd: this.rootPath,
+          maxBuffer: MAX_BUFFER_SIZE, // To solve: stdout maxBuffer exceeded
+          encoding: 'utf-8',
+        },
+        (error, stdout, stderr) => {
+          if (error) {
+            reject(error);
+            return;
+          }
+
+          if (stderr && !stdout) {
+            reject(new Error(stderr));
+            return;
+          }
+
+          resolve({ stdout, stderr });
+        }
+      );
+    });
+  }
+}
---
+++ src/cli/MacCodacyCli.ts
@@ -0,0 +1,175 @@
+import fs from 'fs';
+import path from 'path';
+
+import { CODACY_FOLDER_NAME, CodacyCli } from './CodacyCli.js';
+import { Log } from 'sarif';
+
+export class MacCodacyCli extends CodacyCli {
+  constructor(rootPath: string, provider?: string, organization?: string, repository?: string) {
+    super(rootPath, provider, organization, repository);
+  }
+
+  protected async findCliCommand(): Promise<void> {
+    this.setCliCommand('');
+
+    // check if .codacy/cli.sh exists
+    const localPath = path.join(CODACY_FOLDER_NAME, 'cli.sh');
+    const fullPath = path.join(this.rootPath, localPath);
+
+    if (fs.existsSync(fullPath)) {
+      this.setCliCommand(
+        this._cliVersion ? `CODACY_CLI_VERSION=${this._cliVersion} ${localPath}` : localPath
+      );
+      return;
+    }
+
+    // check if codacy-cli is installed globally
+    try {
+      // first set the command for the getCliCommand to do the right thing when called
+      this.setCliCommand(
+        this._cliVersion ? `CODACY_CLI_VERSION=${this._cliVersion} codacy-cli` : 'codacy-cli'
+      );
+      await this.execAsync(`${this.getCliCommand()} --help`);
+
+      return;
+    } catch {
+      // CLI not found, clear it, and attempt to install it
+      this.setCliCommand('');
+      await this.install();
+      return undefined;
+    }
+  }
+
+  private async preflightCodacyCli(): Promise<void> {
+    // is there a command?
+    if (!this.getCliCommand()) {
+      await this.findCliCommand();
+    } else {
+      await this.initialize();
+    }
+  }
+
+  public async install(): Promise<void> {
+    try {
+      const codacyFolder = path.join(this.rootPath, CODACY_FOLDER_NAME);
+      if (!fs.existsSync(codacyFolder)) {
+        fs.mkdirSync(codacyFolder, { recursive: true });
+      }
+
+      // Download cli.sh if it doesn't exist
+      const codacyCliPath = path.join(CODACY_FOLDER_NAME, 'cli.sh');
+
+      if (!fs.existsSync(codacyCliPath)) {
+        const execPath = this.preparePathForExec(codacyCliPath);
+
+        await this.execAsync(
+          `curl -Ls -o "${execPath}" https://raw.githubusercontent.com/codacy/codacy-cli-v2/main/codacy-cli.sh`
+        );
+
+        await this.execAsync(`chmod +x "${execPath}"`);
+
+        this.setCliCommand(
+          this._cliVersion
+            ? `CODACY_CLI_VERSION=${this._cliVersion} ${codacyCliPath}`
+            : codacyCliPath
+        );
+      }
+    } catch (error) {
+      throw new Error(`Failed to install CLI: ${error}`);
+    }
+
+    // Initialize codacy-cli after installation
+    await this.initialize();
+  }
+
+  public async installDependencies(): Promise<void> {
+    const command = `${this.getCliCommand()} install`;
+    try {
+      await this.execAsync(command);
+    } catch (error) {
+      throw new Error(`Failed to install dependencies: ${error}`);
+    }
+  }
+
+  public async update(): Promise<void> {
+    const command = `${this.getCliCommand()} update`;
+    try {
+      await this.execAsync(command);
+    } catch (error) {
+      throw new Error(`Failed to update CLI: ${error}`);
+    }
+
+    this.installDependencies();
+  }
+
+  public async initialize(): Promise<void> {
+    // Check if the configuration files exist
+    const configFilePath = path.join(this.rootPath, CODACY_FOLDER_NAME, 'codacy.yaml');
+    const toolsFolderPath = path.join(this.rootPath, CODACY_FOLDER_NAME, 'tools-configs');
+
+    let needsInitialization = !fs.existsSync(configFilePath) || !fs.existsSync(toolsFolderPath);
+
+    if (!needsInitialization) {
+      // Check if the mode matches the current properties
+      const cliConfig = fs.readFileSync(
+        path.join(this.rootPath, CODACY_FOLDER_NAME, 'cli-config.yaml'),
+        'utf-8'
+      );
+
+      if (
+        (cliConfig === 'mode: local' && this.repository) ||
+        (cliConfig === 'mode: remote' && !this.repository)
+      ) {
+        needsInitialization = true;
+      }
+    }
+
+    if (needsInitialization) {
+      const apiToken: Record<string, string> = this._accountToken
+        ? { 'api-token': this._accountToken }
+        : {};
+
+      const repositoryAccess: Record<string, string> =
+        this.repository && this.provider && this.organization
+          ? {
+              provider: this.provider,
+              organization: this.organization,
+              repository: this.repository,
+            }
+          : {};
+
+      try {
+        // initialize codacy-cli
+        await this.execAsync(`${this.getCliCommand()} init`, { ...apiToken, ...repositoryAccess });
+      } catch (error) {
+        throw new Error(`Failed to initialize CLI: ${error}`);
+      }
+
+      // install dependencies
+      await this.installDependencies();
+    }
+  }
+
+  public async analyze(options: { file?: string; tool?: string }): Promise<Log | null> {
+    await this.preflightCodacyCli();
+
+    if (!this.getCliCommand()) {
+      throw new Error('CLI command not found. Please install the CLI first.');
+    }
+
+    const { file, tool } = options;
+
+    try {
+      const { stdout } = await this.execAsync(
+        `${this.getCliCommand()} analyze ${file ? this.preparePathForExec(file) : ''} --format sarif`,
+        tool ? { tool: tool } : {}
+      );
+
+      const jsonMatch = /(\{[\s\S]*\}|\[[\s\S]*\])/.exec(stdout);
+
+      return jsonMatch ? (JSON.parse(jsonMatch[0]) as Log) : null;
+    } catch (error) {
+      throw new Error(`Failed to analyze code: ${error}`);
+    }
+  }
+}
